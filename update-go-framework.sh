c8184292129c31fafb12d23fa66426adeb7f6bb6
Matti Dahlbom <matti.dahlbom@qvik.fi>
2018-04-30 10:32:29 +0300

    Added Go client library client.aar + build script

---
+++ update-go-framework.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Manually updates the Go client .aar package. You can automate this process
+# via Android Studio Gradle plugin:
+#
+# https://github.com/golang/go/wiki/Mobile#building-and-deploying-to-android-1
+
+echo "Rebuilding Go SDK framework.."
+
+libdir="`pwd`"
+mygopath="$GOPATH:`pwd`/go-sdk"
+gocodedir="go-sdk/src/github.com/ninchat/ninchat-go/mobile"
+
+cd $gocodedir
+if [ $? -ne 0 ]; then
+    echo "Failed to find go code dir. Did you run this from the ios dir?"
+    exit 1
+fi
+
+echo "Running gomobile tool.."
+GOPATH=$mygopath gomobile bind -target android -o $libdir/client.aar
+if [ $? -ne 0 ]; then
+    echo "gomobile cmd failed, aborting."
+    exit 1
+fi
+
+echo "Done."
