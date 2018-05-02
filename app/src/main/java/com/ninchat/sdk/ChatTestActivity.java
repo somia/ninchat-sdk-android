ea173ecc005455430691674857f643a714d30093
Timo Hintsa <timo.hintsa@qvik.fi>
2018-05-02 16:12:08 +0300

    add simple connection test

--- app/src/main/java/com/ninchat/sdk/ChatTestActivity.java
+++ app/src/main/java/com/ninchat/sdk/ChatTestActivity.java
@@ -2,6 +2,17 @@ package com.ninchat.sdk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ninchat.client.CloseHandler;
+import com.ninchat.client.ConnStateHandler;
+import com.ninchat.client.EventHandler;
+import com.ninchat.client.LogHandler;
+import com.ninchat.client.Payload;
+import com.ninchat.client.Props;
+import com.ninchat.client.Session;
+import com.ninchat.client.SessionEventHandler;
+import com.ninchat.client.Strings;
 
 public class ChatTestActivity extends AppCompatActivity {
 
@@ -9,5 +20,64 @@ public class ChatTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_test);
+
+        test_connection();
+    }
+
+    public void test_connection() {
+        Strings messageTypes = new Strings();
+        messageTypes.append("ninchat.com/*");
+
+        Props sessionParams = new Props();
+        sessionParams.setStringArray("message_types", messageTypes);
+
+        Session session = new Session();
+        session.setOnSessionEvent(new SessionEventHandler() {
+            @Override
+            public void onSessionEvent(Props params) {
+                Log.i("Example", "session_event: " + params.toString());
+            }
+        });
+        session.setOnEvent(new EventHandler() {
+            @Override
+            public void onEvent(Props params, Payload payload, boolean lastReply) {
+                Log.i("Example", "event: " + params.toString());
+            }
+        });
+        session.setOnClose(new CloseHandler() {
+            @Override
+            public void onClose() {
+                Log.i("Example", "closed");
+            }
+        });
+        session.setOnConnState(new ConnStateHandler() {
+            @Override
+            public void onConnState(String state) {
+                Log.d("Example", state);
+            }
+        });
+        session.setOnLog(new LogHandler() {
+            @Override
+            public void onLog(String msg) {
+                Log.d("Client", msg);
+            }
+        });
+
+        try {
+            session.setParams(sessionParams);
+            session.open();
+            Props sendParams = new Props();
+            sendParams.setString("action", "send_message");
+            sendParams.setString("user_id", "007");
+            sendParams.setString("message_type", "ninchat.com/no-such-message-type");
+
+            Payload sendPayload = new Payload();
+            String payload = "{\"text\":\"asdf\"}";
+            sendPayload.append(payload.getBytes());
+
+            session.send(sendParams, sendPayload);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
