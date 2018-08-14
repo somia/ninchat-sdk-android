988676fea181427dcb2f592ef3c9bb2c4ac7dfb4
Jussi Pekonen <jussi.pekonen@qvik.fi>
2018-08-14 10:23:10 +0300

    Update the Go framework build script

--- update-go-framework.sh
+++ update-go-framework.sh
@@ -5,6 +5,25 @@
 #
 # https://github.com/golang/go/wiki/Mobile#building-and-deploying-to-android-1
 
+go=$(which go)
+if [ "$?" != "0" ]; then
+    echo "Go not installed. Aborting."
+    exit 1
+fi
+if [ -z "${GOPATH}" ]; then
+    echo "GOPATH not set. Aborting."
+    exit 1
+fi
+if [ -z "${ANDROID_HOME}" ]; then
+    echo "ANDROID_HOME not set. Aborting."
+    exit 1
+fi
+
+hasgopathinpath=$(echo "${PATH}" | grep "${GOPATH}")
+if [ "$?" != "0" ]; then
+    export PATH="${PATH}:${GOPATH}/bin"
+fi
+
 echo "Rebuilding Go SDK framework.."
 
 libdir="`pwd`/ninchatsdk/libs"
@@ -13,7 +32,7 @@ gocodedir="go-sdk/src/github.com/ninchat/ninchat-go/mobile"
 
 cd $gocodedir
 if [ $? -ne 0 ]; then
-    echo "Failed to find go code dir. Did you run this from the ios dir?"
+    echo "Failed to find go code dir. Did you run this from the Android project dir?"
     exit 1
 fi
 
