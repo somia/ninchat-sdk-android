069a1161cae23841f82d3e159e64a4a8b79e0457
Jussi Pekonen <jussi.pekonen@qvik.fi>
2018-08-17 15:24:56 +0300

    Use data binding in the SDK

--- ninchatsdk/src/main/java/com/ninchat/sdk/activities/BaseActivity.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/activities/BaseActivity.java
@@ -1,5 +1,7 @@
 package com.ninchat.sdk.activities;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
@@ -8,15 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 /**
  * Created by Jussi Pekonen (jussi.pekonen@qvik.fi) on 17/08/2018.
  */
-abstract class BaseActivity extends AppCompatActivity {
+abstract class BaseActivity<VB extends ViewDataBinding> extends AppCompatActivity {
 
     abstract protected @LayoutRes int getLayoutRes();
+
+    protected VB binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(getLayoutRes());
+        binding = DataBindingUtil.setContentView(this, getLayoutRes());
 
     }
 }
