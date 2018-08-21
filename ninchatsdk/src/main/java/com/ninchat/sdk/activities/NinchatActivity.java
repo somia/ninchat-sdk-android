2c3cff918b1afe2a16c450dfddb4aac8cc2627ed
Jussi Pekonen <jussi.pekonen@qvik.fi>
2018-08-21 15:20:18 +0300

    Update the initial view to match the desired look

--- ninchatsdk/src/main/java/com/ninchat/sdk/activities/NinchatActivity.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/activities/NinchatActivity.java
@@ -1,91 +1,26 @@
 package com.ninchat.sdk.activities;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Toast;
 
-import com.ninchat.client.CloseHandler;
-import com.ninchat.client.ConnStateHandler;
-import com.ninchat.client.EventHandler;
-import com.ninchat.client.LogHandler;
-import com.ninchat.client.Payload;
-import com.ninchat.client.Props;
-import com.ninchat.client.Session;
-import com.ninchat.client.SessionEventHandler;
-import com.ninchat.client.Strings;
 import com.ninchat.sdk.R;
 
 
 public class NinchatActivity extends BaseActivity {
-
-    private TextView log;
 
     @Override
     protected int getLayoutRes() {
         return R.layout.activity_ninchat;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        log = findViewById(R.id.log);
-        test();
+    public void onBlogLinkClick(final View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://blog.ninchat.com")));
     }
 
-    private void test() {
-        Strings messageTypes = new Strings();
-        messageTypes.append("ninchat.com/*");
-
-        Props sessionParams = new Props();
-        sessionParams.setStringArray("message_types", messageTypes);
-
-        Session session = new Session();
-        session.setOnSessionEvent(new SessionEventHandler() {
-            @Override
-            public void onSessionEvent(Props params) {
-                log.append("\nsession_event: " + params.toString());
-            }
-        });
-        session.setOnEvent(new EventHandler() {
-            @Override
-            public void onEvent(Props params, Payload payload, boolean lastReply) {
-                log.append("\nevent: " + params.toString());
-            }
-        });
-        session.setOnClose(new CloseHandler() {
-            @Override
-            public void onClose() {
-                log.append("\nsession closed");
-            }
-        });
-        session.setOnConnState(new ConnStateHandler() {
-            @Override
-            public void onConnState(String state) {
-                log.append("\nconnection state: " + state);
-            }
-        });
-        session.setOnLog(new LogHandler() {
-            @Override
-            public void onLog(String msg) {
-                log.append("\nclient: " + msg);
-            }
-        });
-
-        try {
-            session.setParams(sessionParams);
-            session.open();
-            Props sendParams = new Props();
-            sendParams.setString("action", "send_message");
-            sendParams.setString("user_id", "007");
-            sendParams.setString("message_type", "ninchat.com/no-such-message-type");
-
-            Payload sendPayload = new Payload();
-            String payload = "{\"text\":\"asdf\"}";
-            sendPayload.append(payload.getBytes());
-
-            session.send(sendParams, sendPayload);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void onStartButtonClick(final View view) {
+        Toast.makeText(this, "onStartButtonClick", Toast.LENGTH_SHORT).show();
     }
 
 }
