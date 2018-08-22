92af4fa0571fe9146e6bbad17389162f622a9e52
Jussi Pekonen <jussi.pekonen@qvik.fi>
2018-08-22 14:19:06 +0300

    Add feedback view

---
+++ ninchatsdk/src/main/java/com/ninchat/sdk/activities/NinchatReviewActivity.java
@@ -0,0 +1,11 @@
+package com.ninchat.sdk.activities;
+
+import com.ninchat.sdk.R;
+
+public class NinchatReviewActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_ninchat_review;
+    }
+}
