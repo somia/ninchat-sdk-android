ba3ae2eb7b2006135c16ef3571bca118c742a5b3
Jussi Pekonen <jussi.pekonen@qvik.fi>
2018-08-22 16:37:56 +0300

    Add a close button to the toolbar and close the activity when it is tapped

--- ninchatsdk/src/main/java/com/ninchat/sdk/activities/BaseActivity.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/activities/BaseActivity.java
@@ -1,9 +1,14 @@
 package com.ninchat.sdk.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ninchat.sdk.R;
 
 /**
  * Created by Jussi Pekonen (jussi.pekonen@qvik.fi) on 17/08/2018.
@@ -17,5 +22,20 @@ abstract class BaseActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(getLayoutRes());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    protected Intent getOnCloseData() {
+        return null;
+    }
+
+    public void onCloseClick(final MenuItem menuItem) {
+        setResult(Activity.RESULT_CANCELED, getOnCloseData());
+        finish();
     }
 }
