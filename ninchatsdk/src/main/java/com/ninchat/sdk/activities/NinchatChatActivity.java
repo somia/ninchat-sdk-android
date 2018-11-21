527ffbcfeddd496c56c8137c475c0af6d26997e5
Jussi Pekonen <jussi.pekonen@qvik.fi>
2018-11-21 12:49:18 +0200

    Release 0.1.7

--- ninchatsdk/src/main/java/com/ninchat/sdk/activities/NinchatChatActivity.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/activities/NinchatChatActivity.java
@@ -17,7 +17,6 @@ import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
