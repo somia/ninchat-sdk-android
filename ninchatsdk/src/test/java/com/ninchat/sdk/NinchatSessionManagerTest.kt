c22515d57b9092ac4b34fc5a94e6c37987c8d3ea
pallab-gain <pallab.gain@gmail.com>
2020-05-05 11:40:45 +0300

    Added mockitio android specific library Added more unit test for session manager  - audience metadata [ todo ]  - session manager initiatation

--- ninchatsdk/src/test/java/com/ninchat/sdk/NinchatSessionManagerTest.kt
+++ ninchatsdk/src/test/java/com/ninchat/sdk/NinchatSessionManagerTest.kt
@@ -1,5 +1,8 @@
 package com.ninchat.sdk
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import com.ninchat.sdk.NinchatSessionManager.init
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -7,9 +10,10 @@ import org.junit.Test
 class NinchatSessionManagerTest {
 
     lateinit var ninchatSessionManager: NinchatSessionManager
+
     @Before
     fun setUp() {
-        ninchatSessionManager = NinchatSessionManager(null,null,null,null,null, null, null)
+        ninchatSessionManager = NinchatSessionManager(null, null, null, null, null, null, null)
     }
 
     @Test
@@ -34,5 +38,44 @@ class NinchatSessionManagerTest {
         val defaultUserAgent = NinchatSessionManager.DEFAULT_USER_AGENT
         ninchatSessionManager.appDetails = "app-details"
         Assert.assertEquals("$defaultUserAgent app-details", ninchatSessionManager.userAgent)
+    }
+
+    @Test
+    fun `should get default server address`() {
+        val defaultServerAddress = "api.ninchat.com"
+        Assert.assertEquals(defaultServerAddress, ninchatSessionManager.serverAddress)
+    }
+
+    @Test
+    fun `should get expected server address`() {
+        val serverAddress = "test-api.ninchat.com"
+        ninchatSessionManager.serverAddress = serverAddress
+        Assert.assertEquals(serverAddress, ninchatSessionManager.serverAddress)
+    }
+
+    @Test
+    fun `should return null audience metadata`() {
+        Assert.assertNull(ninchatSessionManager.audienceMetadata)
+    }
+
+    @Test
+    fun `should return provided audience metadata`() {
+        // todo pass now - native method mocked
+    }
+
+    @Test
+    fun `should return empty NinchatSessionManager instance if init is not called`() {
+        Assert.assertNull(NinchatSessionManager.getInstance())
+    }
+
+    @Test
+    fun `should return non empty NinchatSessionManager instance after init is called`() {
+        NinchatSessionManager.init(null, null, null, null, null, null, null)
+        Assert.assertNotNull(NinchatSessionManager.getInstance())
+    }
+
+    @Test
+    fun `copyme_test_template`() {
+        // todo implements me
     }
 }
