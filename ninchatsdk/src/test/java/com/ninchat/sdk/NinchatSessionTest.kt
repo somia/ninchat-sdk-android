0bb2eeb7229122c5cd21ecb07aab264e33cb6716
pallab-gain <pallab.gain@gmail.com>
2020-05-06 10:26:40 +0300

    List of test that will be coverd for ninchat session unit test

---
+++ ninchatsdk/src/test/java/com/ninchat/sdk/NinchatSessionTest.kt
@@ -0,0 +1,76 @@
+package com.ninchat.sdk
+
+import org.junit.Test
+
+class NinchatSessionTest {
+    @Test
+    fun `should create ninchat session with application context and configuration key`() {
+    }
+
+    @Test
+    fun `should set ninchat session credentials`() {
+    }
+
+    @Test
+    fun `should set ninchat configuration`() {
+    }
+
+    @Test
+    fun `should set ninchat preferred environments`() {
+    }
+
+    @Test
+    fun `should set ninchat event listener`() {
+    }
+
+    @Test
+    fun `should set ninchat log listeners`() {
+    }
+
+    @Test
+    fun `should set ninchat site address`() {
+    }
+
+    @Test
+    fun `should start session with activity`() {
+    }
+
+    @Test
+    fun `should start session with activity and request code`() {
+    }
+
+    @Test
+    fun `should start session with activity, and queue id`() {
+    }
+
+    @Test
+    fun `should start session with activity, request code and queue id`() {
+    }
+
+    //todo: separate ratting class and move test from here
+
+    @Test
+    fun `should be equal to GOOD rating`() {
+        // todo implements me
+    }
+
+    @Test
+    fun `should be equal to FAIR rating`() {
+        // todo implements me
+    }
+
+    @Test
+    fun `should be equal to POOR rating`() {
+        // todo implements me
+    }
+
+    @Test
+    fun `should be equal to NO_ANSWER`() {
+        // todo implements me
+    }
+
+    @Test
+    fun `copyme_test_template`() {
+        // todo implements me
+    }
+}
