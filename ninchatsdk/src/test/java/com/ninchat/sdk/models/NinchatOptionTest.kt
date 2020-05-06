1c178ef232e7ea77affa994f9aff4b51a8398825
pallab-gain <pallab.gain@gmail.com>
2020-05-07 01:25:42 +0300

    Test for Ninchat option

--- ninchatsdk/src/test/java/com/ninchat/sdk/models/NinchatOptionTest.kt
+++ ninchatsdk/src/test/java/com/ninchat/sdk/models/NinchatOptionTest.kt
@@ -1,4 +1,34 @@
 package com.ninchat.sdk.models
 
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.*
+
 class NinchatOptionTest {
+    @Test
+    fun `should initial NinchatOption with option JSON`() {
+        val mockedJson = mock(JSONObject::class.java)
+        `when`(mockedJson.getString("label")).thenReturn("test-label")
+        val ninchatOption = NinchatOption(mockedJson)
+        Assert.assertEquals("test-label", ninchatOption.label)
+    }
+
+    @Test
+    fun `should initialize with selected false`() {
+        val mockedJson = mock(JSONObject::class.java)
+        val ninchatOption = NinchatOption(mockedJson)
+        Assert.assertEquals(false, ninchatOption.isSelected)
+    }
+
+    @Test
+    fun `should be able to toggle selected value`() {
+        val mockedJson = mock(JSONObject::class.java)
+        val ninchatOption = NinchatOption(mockedJson)
+        Assert.assertEquals(false, ninchatOption.isSelected)
+        ninchatOption.toggle()
+        Assert.assertEquals(true, ninchatOption.isSelected)
+    }
+
 }
