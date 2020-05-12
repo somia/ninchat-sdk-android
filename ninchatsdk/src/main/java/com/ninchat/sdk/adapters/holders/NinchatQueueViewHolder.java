2ab5b07e808849e93c7f9ae70dbd66d652dce08c
pallab-gain <pallab.gain@gmail.com>
2020-05-13 01:52:51 +0300

    Unit test ninchat queue view holder

--- ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/NinchatQueueViewHolder.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/NinchatQueueViewHolder.java
@@ -14,26 +14,37 @@ public class NinchatQueueViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(final NinchatQueue queue, final Callback callback) {
-        final Button button = itemView.findViewById(R.id.queue_name);
-
+        final Button button = this.getButtonItem();
         // If queue is closed, disable button and set alpha for look & feel
         if (queue.isClosed()) {
             button.setEnabled(false);
             button.setAlpha(0.5f);
-            button.setText(NinchatSessionManager
-                    .getInstance()
-                    .getQueueName(queue.getName(), queue.isClosed()));
+            button.setText(this.getText(queue));
         } else {
             button.setAlpha(1f);
-            button.setText(NinchatSessionManager
-                    .getInstance()
-                    .getQueueName(queue.getName()));
+            button.setText(this.getText(queue));
             button.setOnClickListener(v -> {
                 callback.onClickListener(queue.getId());
             });
         }
     }
 
+    public String getText(final NinchatQueue queue) {
+        if (queue == null) return null;
+        if (queue.isClosed()) {
+            NinchatSessionManager
+                    .getInstance()
+                    .getQueueName(queue.getName(), queue.isClosed());
+        }
+        return NinchatSessionManager
+                .getInstance()
+                .getQueueName(queue.getName());
+
+    }
+
+    public Button getButtonItem() {
+        return itemView.findViewById(R.id.queue_name);
+    }
 
     public interface Callback {
         void onClickListener(String queueId);
