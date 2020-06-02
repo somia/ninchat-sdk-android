efaf688fbafcd503b1b2abdbfbeb8be78253721a
pallab-gain <pallab.gain@gmail.com>
2020-06-02 15:54:16 +0300

    Added form recycler view  - Multichoice view item

---
+++ ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatMultichoiceViewHolder.java
@@ -0,0 +1,30 @@
+package com.ninchat.sdk.adapters.holders.formview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ninchat.sdk.R;
+
+import org.json.JSONObject;
+
+public class NinchatMultichoiceViewHolder extends RecyclerView.ViewHolder {
+    private final String TAG = NinchatMultichoiceViewHolder.class.getSimpleName();
+
+    private final TextView mLabel;
+    private final TextView mContent;
+
+    public NinchatMultichoiceViewHolder(@NonNull View itemView, final JSONObject item) {
+        super(itemView);
+        mLabel = (TextView) itemView.findViewById(R.id.multichoice_label);
+        mContent = (TextView) itemView.findViewById(R.id.text_view_content);
+        this.bind(item);
+    }
+
+
+    public void bind(JSONObject item) {
+        mLabel.setText(item.optString("name", ""));
+        mContent.setText(item.optString("label", ""));
+    }
+}
