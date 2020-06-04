7bf35492d73f488c199813fd7e6ac334a87122d3
pallab-gain <pallab.gain@gmail.com>
2020-06-04 10:15:13 +0300

    Added form recycler view  - Radio button view

--- ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatRadioButtonViewHolder.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatRadioButtonViewHolder.java
@@ -1,9 +1,11 @@
 package com.ninchat.sdk.adapters.holders.formview;
 
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -18,6 +20,10 @@ public class NinchatRadioButtonViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView mLabel;
     private final RadioGroup mRadioGroup;
+    private final float density = itemView.getResources().getDisplayMetrics().density;
+    private final int marginStartInDp = 8;
+    private final int marginBottomInDp = 5;
+    private final int paddingStartInDp = 10;
 
     public NinchatRadioButtonViewHolder(@NonNull View itemView, final JSONObject item) {
         super(itemView);
@@ -33,6 +39,12 @@ public class NinchatRadioButtonViewHolder extends RecyclerView.ViewHolder {
         if (options == null) {
             return;
         }
+        int marginStart = (int) (marginStartInDp * density);
+        int marginBottom = (int) (marginBottomInDp * density);
+        int paddingStart = (int) (paddingStartInDp * density);
+
+        RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(marginStart, 0, 0, marginBottom);
         for (int i = 0; i < options.length(); i += 1) {
             final JSONObject curOption = options.optJSONObject(i);
             final String label = curOption.optString("label");
@@ -40,6 +52,8 @@ public class NinchatRadioButtonViewHolder extends RecyclerView.ViewHolder {
             final RadioButton btn = new RadioButton(itemView.getContext());
             btn.setId(View.generateViewId());
             btn.setText(label);
+            btn.setLayoutParams(params);
+            btn.setPadding(paddingStart, 0, 0, 0);
             mRadioGroup.addView(btn);
             mRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
                 Log.d(TAG, value);
