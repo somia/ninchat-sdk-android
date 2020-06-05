0725efe3945c57b70c3090c4b266a6bad559700a
pallab-gain <pallab.gain@gmail.com>
2020-06-05 15:16:08 +0300

    Added form recycler view  - Checkbox color and state sync

--- ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatCheckboxViewHolder.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatCheckboxViewHolder.java
@@ -1,26 +1,20 @@
 package com.ninchat.sdk.adapters.holders.formview;
 
 import android.support.annotation.NonNull;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.CompoundButton;
 
 import com.ninchat.sdk.R;
-
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class NinchatCheckboxViewHolder extends RecyclerView.ViewHolder {
     private final String TAG = NinchatCheckboxViewHolder.class.getSimpleName();
 
     private final CheckBox mCheckbox;
+    private boolean checked;
 
     public NinchatCheckboxViewHolder(@NonNull View itemView, final JSONObject item) {
         super(itemView);
@@ -30,5 +24,19 @@ public class NinchatCheckboxViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(JSONObject item) {
         mCheckbox.setText(item.optString("label", ""));
+        mCheckbox.setOnCheckedChangeListener(onCheckedChangeListener);
+    }
+
+    public CompoundButton.OnCheckedChangeListener onCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            checked = isChecked;
+            onCheckBoxStateChanged(isChecked);
+        }
+    };
+
+    public void onCheckBoxStateChanged(final boolean isChecked) {
+        mCheckbox.setTextColor(ContextCompat.getColor(itemView.getContext(),
+                isChecked ? R.color.checkbox_text_selected : R.color.checkbox_text_not_selected));
     }
 }
