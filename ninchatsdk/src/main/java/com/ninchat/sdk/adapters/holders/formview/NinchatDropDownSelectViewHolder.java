ac15fb88072570a4a1d5a4b47121ca3c2e07329f
pallab-gain <pallab.gain@gmail.com>
2020-06-08 13:44:24 +0300

    Added form recycler view  - Dropdown selector view and logic

--- ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatDropDownSelectViewHolder.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatDropDownSelectViewHolder.java
@@ -1,11 +1,14 @@
 package com.ninchat.sdk.adapters.holders.formview;
 
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -19,6 +22,7 @@ public class NinchatDropDownSelectViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView mLabel;
     private final Spinner mSpinner;
+    private int selectedIndex;
 
     public NinchatDropDownSelectViewHolder(@NonNull View itemView, final JSONObject item) {
         super(itemView);
@@ -30,6 +34,7 @@ public class NinchatDropDownSelectViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(JSONObject item) {
         mLabel.setText(item.optString("label", ""));
+        selectedIndex = 0;
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(itemView.getContext(), android.R.layout.simple_spinner_dropdown_item);
         final JSONArray options = item.optJSONArray("options");
         if (options == null) {
@@ -42,11 +47,18 @@ public class NinchatDropDownSelectViewHolder extends RecyclerView.ViewHolder {
             final String value = curOption.optString("value");
             dataAdapter.add(label);
         }
+        mSpinner.setSelection(selectedIndex);
         mSpinner.setAdapter(dataAdapter);
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.d(TAG, ""+position+" "+id);
+                Log.d(TAG, "" + position + " " + id);
+                selectedIndex = position;
+                if (position != 0) {
+                    onSelected(true);
+                } else {
+                    onSelected(false);
+                }
             }
 
             @Override
@@ -54,5 +66,16 @@ public class NinchatDropDownSelectViewHolder extends RecyclerView.ViewHolder {
                 Log.d(TAG, "nothing is selected");
             }
         });
+    }
+
+    public void onSelected(boolean isSelected) {
+        ((RelativeLayout) itemView.findViewById(R.id.dropdown_select_layout)).setBackground(
+                ContextCompat.getDrawable(itemView.getContext(),
+                        isSelected ? R.drawable.ninchat_dropdown_select : R.drawable.ninchat_dropdown_not_selected));
+
+        ((ImageView) itemView.findViewById(R.id.ninchat_dropdown_list_icon)).setColorFilter(
+                ContextCompat.getColor(itemView.getContext(),
+                        isSelected ? R.color.checkbox_selected : R.color.checkbox_not_selected)
+        );
     }
 }
