eef3ec5682a3515d95ae2f6e8fbc13afd42e518b
pallab-gain <pallab.gain@gmail.com>
2020-06-09 16:01:20 +0300

    Added form recycler view  - Can render and show image in the text view

--- ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatTextViewHolder.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/adapters/holders/formview/NinchatTextViewHolder.java
@@ -1,24 +1,21 @@
 package com.ninchat.sdk.adapters.holders.formview;
 
 import android.annotation.SuppressLint;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.ninchat.sdk.R;
+import com.ninchat.sdk.helper.NinchatImageGetter;
 import com.ninchat.sdk.helper.NinchatQuestionnaire;
 
 import org.json.JSONObject;
 
 public class NinchatTextViewHolder extends RecyclerView.ViewHolder {
     private final String TAG = NinchatTextViewHolder.class.getSimpleName();
-
     private final TextView mContent;
 
     public NinchatTextViewHolder(@NonNull View itemView, final JSONObject item) {
@@ -28,11 +25,11 @@ public class NinchatTextViewHolder extends RecyclerView.ViewHolder {
     }
 
 
-    @SuppressLint("NewApi")
     public void bind(JSONObject item) {
         final String text = item.optString("label", "");
         mContent.setAutoLinkMask(0);
-        mContent.setText(NinchatQuestionnaire.fromHTML(text));
         mContent.setMovementMethod(LinkMovementMethod.getInstance());
+        // there are images
+        mContent.setText(Html.fromHtml(text, new NinchatImageGetter(mContent, true, null), null));
     }
 }
