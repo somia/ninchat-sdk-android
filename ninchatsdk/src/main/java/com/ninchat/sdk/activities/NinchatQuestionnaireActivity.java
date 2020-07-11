3aa94c05559ab05efcf59e285093079337fab504
pallab-gain <pallab.gain@gmail.com>
2020-07-11 13:15:33 +0300

    Renamed xml elements

--- ninchatsdk/src/main/java/com/ninchat/sdk/activities/NinchatQuestionnaireActivity.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/activities/NinchatQuestionnaireActivity.java
@@ -3,14 +3,12 @@ package com.ninchat.sdk.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -274,9 +272,9 @@ public final class NinchatQuestionnaireActivity extends NinchatBaseActivity {
                 new NinchatQuestionnaire(
                         getElements(
                                 getQuestionnaire(historyList.peek()))));
-        final int spaceInPixelTop = getResources().getDimensionPixelSize(R.dimen.items_margin_top);
-        final int spaceLeft = getResources().getDimensionPixelSize(R.dimen.items_margin_left);
-        final int spaceRight = getResources().getDimensionPixelSize(R.dimen.items_margin_right);
+        final int spaceInPixelTop = getResources().getDimensionPixelSize(R.dimen.ninchat_items_margin_top);
+        final int spaceLeft = getResources().getDimensionPixelSize(R.dimen.ninchat_items_margin_left);
+        final int spaceRight = getResources().getDimensionPixelSize(R.dimen.ninchat_items_margin_right);
         mRecyclerView.addItemDecoration(new NinchatQuestionnaireItemDecoration(
                 spaceInPixelTop,
                 spaceLeft,
@@ -289,9 +287,9 @@ public final class NinchatQuestionnaireActivity extends NinchatBaseActivity {
     private void createConversationLikeQuestionnaire() {
         mConversationLikeQuestionnaireAdapter = new NinchatConversationLikeQuestionnaireAdapter(
                 new NinchatQuestionnaire(getQuestionnaireList()));
-        final int spaceInPixelTop = getResources().getDimensionPixelSize(R.dimen.items_margin_top);
-        final int spaceLeft = getResources().getDimensionPixelSize(R.dimen.items_margin_left);
-        final int spaceRight = getResources().getDimensionPixelSize(R.dimen.items_margin_right);
+        final int spaceInPixelTop = getResources().getDimensionPixelSize(R.dimen.ninchat_items_margin_top);
+        final int spaceLeft = getResources().getDimensionPixelSize(R.dimen.ninchat_items_margin_left);
+        final int spaceRight = getResources().getDimensionPixelSize(R.dimen.ninchat_items_margin_right);
         mRecyclerView.addItemDecoration(new NinchatQuestionnaireItemDecoration(
                 spaceInPixelTop,
                 spaceLeft,
