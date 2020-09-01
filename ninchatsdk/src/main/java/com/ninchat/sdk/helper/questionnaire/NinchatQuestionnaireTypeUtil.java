888dab1d4ca2112cf5a81dfe8ae15fb36d0b21fe
Pallab <1187721+pallab-gain@users.noreply.github.com>
2020-09-01 15:32:03 +0300

    265/resume session for closed queue (#147)

--- ninchatsdk/src/main/java/com/ninchat/sdk/helper/questionnaire/NinchatQuestionnaireTypeUtil.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/helper/questionnaire/NinchatQuestionnaireTypeUtil.java
@@ -21,7 +21,7 @@ public class NinchatQuestionnaireTypeUtil {
     public static final int POST_AUDIENCE_QUESTIONNAIRE = 2;
     public static final int DEFAULT_INT_VALUE = -1;
 
-    public static final int NEW_SESSION = 1;
+    public static final int NEW_SESSION = 0;
     public static final int IN_QUEUE = 2;
     public static final int HAS_CHANNEL = 3;
     public static final int NONE = 4;
