aef33fe19cb4ea6da4558965ba494b3959e735c8
pallab-gain <pallab.gain@gmail.com>
2020-09-17 10:27:54 +0300

    Site config class  - Generaö String, boolean, Array parser  - JSON to site config object getter

--- ninchatsdk/src/main/java/com/ninchat/sdk/helper/siteconfigparser/NinchatSiteConfig.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/helper/siteconfigparser/NinchatSiteConfig.kt
@@ -106,4 +106,76 @@ class NinchatSiteConfig {
 
     fun showAgentAvatar(preferredEnvironments: ArrayList<String>?): Boolean =
             getBoolean("agentAvatar", preferredEnvironments) ?: false
+
+    fun getAgentAvatar(preferredEnvironments: ArrayList<String>?): String? =
+            getString("agentAvatar", preferredEnvironments)
+
+    fun getConversationEndedText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Conversation ended", preferredEnvironments) ?: "Conversation ended"
+
+    fun getChatCloseText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Close chat", preferredEnvironments) ?: "Close chat"
+
+    fun getChatCloseDescriptionText(preferredEnvironments: ArrayList<String>?): String =
+            getString("closeConfirmText", preferredEnvironments) ?: "closeConfirmText"
+
+    fun getContinueChatText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Continue chat", preferredEnvironments) ?: "Continue chat"
+
+    fun getEnterMessageText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Enter your message", preferredEnvironments) ?: "Enter your message"
+
+    fun getVideoChatTitleText(preferredEnvironments: ArrayList<String>?): String =
+            getString("You are invited to a video chat", preferredEnvironments)
+                    ?: "You are invited to a video chat"
+
+    fun getVideoChatDescriptionText(preferredEnvironments: ArrayList<String>?): String =
+            getString("wants to video chat with you", preferredEnvironments)
+                    ?: "wants to video chat with you"
+
+    fun getVideoCallAcceptText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Accept", preferredEnvironments) ?: "Accept"
+
+    fun getVideoCallDeclineText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Decline", preferredEnvironments) ?: "Decline"
+
+    fun getVideoCallMetaMessageText(preferredEnvironments: ArrayList<String>?): String =
+            getString("You are invited to a video chat", preferredEnvironments)
+                    ?: "You are invited to a video chat"
+
+    fun getVideoCallAcceptedText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Video chat answered", preferredEnvironments) ?: "Video chat answered"
+
+    fun getVideoCallRejectedText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Video chat declined", preferredEnvironments) ?: "Video chat declined"
+
+    fun getMOTDText(preferredEnvironments: ArrayList<String>?): String =
+            getString("motd", preferredEnvironments) ?: "motd"
+
+    fun getQueueMessageText(preferredEnvironments: ArrayList<String>?): String =
+            getString("inQueueText", preferredEnvironments) ?: "inQueueText"
+
+
+    fun showRating(preferredEnvironments: ArrayList<String>?): Boolean =
+            getBoolean("audienceRating", preferredEnvironments) ?: false
+
+    fun getFeedbackTitleText(preferredEnvironments: ArrayList<String>?): String =
+            getString("How was our customer service?", preferredEnvironments)
+                    ?: "How was our customer service?"
+
+    fun getThankYouTextText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Thank you for the conversation!", preferredEnvironments)
+                    ?: "Thank you for the conversation!"
+
+    fun getFeedbackPositiveText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Good", preferredEnvironments) ?: "Good"
+
+    fun getFeedbackNeutralText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Okay", preferredEnvironments) ?: "Okay"
+
+    fun getFeedbackNegativeText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Poor", preferredEnvironments) ?: "Poor"
+
+    fun getFeedbackSkipText(preferredEnvironments: ArrayList<String>?): String =
+            getString("Skip", preferredEnvironments) ?: "Skip"
 }
