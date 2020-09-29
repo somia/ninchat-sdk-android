a0b1d1184bf17df26a7014d35ec657fbc90c92a8
pallab-gain <pallab.gain@gmail.com>
2020-09-29 13:05:47 +0300

    Remove un-used ninchat avatar class Convert questionnaire iterm decoration class to kotlin class

--- ninchatsdk/src/main/java/com/ninchat/sdk/helper/NinchatQuestionnaireItemDecoration.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/helper/NinchatQuestionnaireItemDecoration.kt
@@ -1,24 +1,14 @@
-package com.ninchat.sdk.helper;
+package com.ninchat.sdk.helper
 
-import android.graphics.Rect;
-import androidx.recyclerview.widget.RecyclerView;
-import android.view.View;
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 
-public class NinchatQuestionnaireItemDecoration extends RecyclerView.ItemDecoration {
-    private int spaceTop;
-    private int spaceLeft;
-    private int spaceRight;
-
-    public NinchatQuestionnaireItemDecoration(int spaceTop, int spaceLeft, int spaceRight) {
-        this.spaceTop = spaceTop;
-        this.spaceLeft = spaceLeft;
-        this.spaceRight = spaceRight;
-    }
-
-    @Override
-    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        outRect.top = spaceTop;
-        outRect.left = spaceLeft;
-        outRect.right = spaceRight;
+class NinchatQuestionnaireItemDecoration(private val spaceTop: Int, private val spaceLeft: Int, private val spaceRight: Int) : ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.top = spaceTop
+        outRect.left = spaceLeft
+        outRect.right = spaceRight
     }
 }
