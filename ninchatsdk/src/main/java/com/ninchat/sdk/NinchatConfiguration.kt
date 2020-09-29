d0cf3c32023c00161aaf07a8d172047f9b89e795
pallab-gain <pallab.gain@gmail.com>
2020-09-29 13:22:52 +0300

    Convert class to kotlin data class  - NinchatConfiguration.kt

--- ninchatsdk/src/main/java/com/ninchat/sdk/NinchatConfiguration.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/NinchatConfiguration.kt
@@ -1,7 +1,4 @@
-package com.ninchat.sdk;
-
-
-import androidx.annotation.Nullable;
+package com.ninchat.sdk
 
 /**
  * Ninchat configuration
@@ -10,36 +7,21 @@ import androidx.annotation.Nullable;
  * The current implementation can change the caller user name attribute so that user can sdk user
  * can change user name during set up
  */
-public class NinchatConfiguration {
-    @Nullable
-    private String userName;
+class NinchatConfiguration private constructor() {
+    var userName: String? = null
+        private set
 
-    private NinchatConfiguration() {
-        // pass
-    }
-
-    private void setUserName(@Nullable String userName) {
-        this.userName = userName;
-    }
-
-    @Nullable
-    public String getUserName() {
-        return userName;
-    }
-
-    public static class Builder {
-        @Nullable
-        private String userName;
-
-        public Builder setUserName(String userName) {
-            this.userName = userName;
-            return this;
+    class Builder {
+        private var userName: String? = null
+        fun setUserName(userName: String?): Builder {
+            this.userName = userName
+            return this
         }
 
-        public NinchatConfiguration create() {
-            NinchatConfiguration configurationManager = new NinchatConfiguration();
-            configurationManager.setUserName(this.userName);
-            return configurationManager;
+        fun create(): NinchatConfiguration {
+            val configurationManager = NinchatConfiguration()
+            configurationManager.userName = userName
+            return configurationManager
         }
     }
 }
