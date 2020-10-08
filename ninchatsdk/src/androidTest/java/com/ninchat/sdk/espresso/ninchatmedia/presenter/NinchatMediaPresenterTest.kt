bbd6eb6aa30f251d3e0c63872092d99c8d03ea78
pallab-gain <pallab.gain@gmail.com>
2020-10-08 09:10:03 +0300

    Instrumentation test  - NinchatMediaPresenterTest

---
+++ ninchatsdk/src/androidTest/java/com/ninchat/sdk/espresso/ninchatmedia/presenter/NinchatMediaPresenterTest.kt
@@ -0,0 +1,12 @@
+package com.ninchat.sdk.espresso.ninchatmedia.presenter
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class NinchatMediaPresenterTest {
+
+}
