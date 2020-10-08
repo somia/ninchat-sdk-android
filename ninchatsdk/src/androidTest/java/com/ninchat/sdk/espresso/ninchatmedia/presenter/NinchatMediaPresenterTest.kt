48aba042d6d71574471bc36751a83e82d66f131f
pallab-gain <pallab.gain@gmail.com>
2020-10-08 09:13:17 +0300

    Instrumentation test  - test skeleton methods for NinchatMediaPresenterTest

--- ninchatsdk/src/androidTest/java/com/ninchat/sdk/espresso/ninchatmedia/presenter/NinchatMediaPresenterTest.kt
+++ ninchatsdk/src/androidTest/java/com/ninchat/sdk/espresso/ninchatmedia/presenter/NinchatMediaPresenterTest.kt
@@ -2,11 +2,33 @@ package com.ninchat.sdk.espresso.ninchatmedia.presenter
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class NinchatMediaPresenterTest {
+
+    @Test
+    fun should_update_file_id_from_intent() {
+
+    }
+
+    @Test
+    fun should_toggle_visibility_of_given_view() {
+
+    }
+
+    @Test
+    fun fetch_launch_intent_with_file_id() {
+
+    }
+
+    @Test
+    fun should_download_a_file_using_native_download_manager() {
+
+    }
+
 
 }
