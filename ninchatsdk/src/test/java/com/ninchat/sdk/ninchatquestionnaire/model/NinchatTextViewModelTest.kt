7350297589683ca93837d949b8e21021ff62a28c
pallab-gain <pallab.gain@gmail.com>
2020-10-16 12:31:43 +0300

    Ninchat text view refactoring  - Unit test for ninchat text view model

---
+++ ninchatsdk/src/test/java/com/ninchat/sdk/ninchatquestionnaire/model/NinchatTextViewModelTest.kt
@@ -0,0 +1,39 @@
+package com.ninchat.sdk.ninchatquestionnaire.model
+
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Test
+
+class NinchatTextViewModelTest {
+    @Test
+    fun `should create a default ninchat text view model`(){
+        val ninchatTextViewModel = NinchatTextViewModel()
+        Assert.assertEquals("", ninchatTextViewModel.label)
+        Assert.assertEquals(true, ninchatTextViewModel.isFormLikeQuestionnaire)
+    }
+
+    @Test
+    fun `should parse optional label from json payload`(){
+        val jsonObject = JSONObject("""{
+            "label": "sample label"
+        }""".trimIndent())
+        val ninchatTextViewModel = NinchatTextViewModel(
+                isFormLikeQuestionnaire = false
+        ).parse(jsonObject)
+        Assert.assertEquals("sample label", ninchatTextViewModel.label)
+        Assert.assertEquals(false, ninchatTextViewModel.isFormLikeQuestionnaire)
+    }
+
+    @Test
+    fun `should parse mandatory label from json payload`(){
+        val jsonObject = JSONObject("""{
+            "label": "sample label",
+            "required": true
+        }""".trimIndent())
+        val ninchatTextViewModel = NinchatTextViewModel(
+                isFormLikeQuestionnaire = false
+        ).parse(jsonObject)
+        Assert.assertEquals("sample label *", ninchatTextViewModel.label)
+        Assert.assertEquals(false, ninchatTextViewModel.isFormLikeQuestionnaire)
+    }
+}
