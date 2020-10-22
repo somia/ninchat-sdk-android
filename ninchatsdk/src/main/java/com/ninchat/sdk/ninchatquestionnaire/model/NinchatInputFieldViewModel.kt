02f8b3628419ff9da121dbeb63323f90b0c7846e
pallab-gain <pallab.gain@gmail.com>
2020-10-22 12:24:53 +0300

    Refactor NinchatInputFieldViewModel  - Data class for input field view and presenter handler

---
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatquestionnaire/model/NinchatInputFieldViewModel.kt
@@ -0,0 +1,7 @@
+package com.ninchat.sdk.ninchatquestionnaire.model
+
+data class NinchatInputFieldViewModel(
+        val label: String? = "",
+        val value: String? = "",
+        val hasError: Boolean = false,
+)
