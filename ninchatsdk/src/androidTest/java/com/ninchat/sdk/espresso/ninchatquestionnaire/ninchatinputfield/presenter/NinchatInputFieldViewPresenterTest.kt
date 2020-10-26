6ef97abb332619e9794be9bceed45fc29cc3e1e4
pallab-gain <pallab.gain@gmail.com>
2020-10-26 16:47:58 +0200

    Add instrumentation test for NinchatInputFieldViewPresenter

--- ninchatsdk/src/androidTest/java/com/ninchat/sdk/espresso/ninchatquestionnaire/ninchatinputfield/presenter/NinchatInputFieldViewPresenterTest.kt
+++ ninchatsdk/src/androidTest/java/com/ninchat/sdk/espresso/ninchatquestionnaire/ninchatinputfield/presenter/NinchatInputFieldViewPresenterTest.kt
@@ -1,10 +1,188 @@
 package com.ninchat.sdk.espresso.ninchatquestionnaire.ninchatinputfield.presenter
 
+import com.ninchat.sdk.ninchatquestionnaire.ninchatinputfield.presenter.INinchatInputFieldViewPresenter
+import com.ninchat.sdk.ninchatquestionnaire.ninchatinputfield.presenter.NinchatInputFieldViewPresenter
+import org.json.JSONObject
+import org.junit.Assert
 import org.junit.Test
 
 class NinchatInputFieldViewPresenterTest {
-    @Test
-    fun `should`() {
 
+    @Test
+    fun `should_render_form_view_with_given_params`() {
+        val jsonObject = JSONObject("""{
+            "result": "test-result",
+            "pattern": "test-pattern",
+            "label": "test-label",
+            "hasError": false
+        }""".trimIndent())
+
+        val viewCallback = object : INinchatInputFieldViewPresenter {
+            override fun onUpdateFromView(label: String) {
+                Assert.assertEquals("test-label", label)
+            }
+
+            override fun onUpdateConversationView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateText(value: String, hasError: Boolean) {
+                Assert.assertEquals("test-result", value)
+                Assert.assertEquals(false, hasError)
+            }
+
+            override fun onUpdateFocus(hasFocus: Boolean) {
+                Assert.assertEquals(false, hasFocus)
+            }
+        }
+        val ninchatInputFieldViewPresenter = NinchatInputFieldViewPresenter(
+                jsonObject = jsonObject,
+                isMultiline = false,
+                isFormLikeQuestionnaire = true,
+                viewCallback = viewCallback)
+
+        ninchatInputFieldViewPresenter.renderCurrentView()
+    }
+
+    @Test
+    fun `should_render_conversation_view_with_given_params`() {
+        val jsonObject = JSONObject("""{
+            "result": "test-result",
+            "pattern": "test-pattern",
+            "label": "test-label",
+            "hasError": false
+        }""".trimIndent())
+
+        val viewCallback = object : INinchatInputFieldViewPresenter {
+            override fun onUpdateFromView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateConversationView(label: String) {
+                Assert.assertEquals("test-label", label)
+            }
+
+            override fun onUpdateText(value: String, hasError: Boolean) {
+                Assert.assertEquals("test-result", value)
+                Assert.assertEquals(false, hasError)
+            }
+
+            override fun onUpdateFocus(hasFocus: Boolean) {
+                Assert.assertEquals(false, hasFocus)
+            }
+        }
+        val ninchatInputFieldViewPresenter = NinchatInputFieldViewPresenter(
+                jsonObject = jsonObject,
+                isMultiline = false,
+                isFormLikeQuestionnaire = false,
+                viewCallback = viewCallback)
+
+        ninchatInputFieldViewPresenter.renderCurrentView()
+    }
+
+    @Test
+    fun `should_call_onTextChange_callback_with_proper_params`() {
+        val jsonObject = JSONObject("""{
+            "result": "test-result",
+            "pattern": "test-pattern",
+            "label": "test-label",
+            "hasError": false
+        }""".trimIndent())
+
+        val viewCallback = object : INinchatInputFieldViewPresenter {
+            override fun onUpdateFromView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateConversationView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateText(value: String, hasError: Boolean) {
+                Assert.assertEquals("new-text", value)
+                Assert.assertEquals(false, hasError)
+            }
+
+            override fun onUpdateFocus(hasFocus: Boolean) {
+                Assert.assertEquals("Should not be called", true)
+            }
+        }
+        val ninchatInputFieldViewPresenter = NinchatInputFieldViewPresenter(
+                jsonObject = jsonObject,
+                isMultiline = false,
+                isFormLikeQuestionnaire = false,
+                viewCallback = viewCallback)
+
+        ninchatInputFieldViewPresenter.onTextChange("new-text")
+    }
+
+    @Test
+    fun `should_call_onFocusChange_callback_when_there_is_no_error`() {
+        val jsonObject = JSONObject("""{
+            "result": "test-result",
+            "pattern": "test-pattern",
+            "label": "test-label",
+            "hasError": false
+        }""".trimIndent())
+
+        val viewCallback = object : INinchatInputFieldViewPresenter {
+            override fun onUpdateFromView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateConversationView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateText(value: String, hasError: Boolean) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateFocus(hasFocus: Boolean) {
+                Assert.assertEquals(true, true)
+            }
+        }
+        val ninchatInputFieldViewPresenter = NinchatInputFieldViewPresenter(
+                jsonObject = jsonObject,
+                isMultiline = false,
+                isFormLikeQuestionnaire = false,
+                viewCallback = viewCallback)
+
+        ninchatInputFieldViewPresenter.onFocusChange(true)
+    }
+
+    @Test
+    fun `should_not_call_onFocusChange_callback_when_there_is_error`() {
+        val jsonObject = JSONObject("""{
+            "result": "test-result",
+            "pattern": "test-pattern",
+            "label": "test-label",
+            "hasError": true
+        }""".trimIndent())
+
+        val viewCallback = object : INinchatInputFieldViewPresenter {
+            override fun onUpdateFromView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateConversationView(label: String) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateText(value: String, hasError: Boolean) {
+                Assert.assertEquals("Should not be called", true)
+            }
+
+            override fun onUpdateFocus(hasFocus: Boolean) {
+                Assert.assertEquals("Should not be called", true)
+            }
+        }
+        val ninchatInputFieldViewPresenter = NinchatInputFieldViewPresenter(
+                jsonObject = jsonObject,
+                isMultiline = false,
+                isFormLikeQuestionnaire = false,
+                viewCallback = viewCallback)
+
+        ninchatInputFieldViewPresenter.onFocusChange(true)
     }
 }
