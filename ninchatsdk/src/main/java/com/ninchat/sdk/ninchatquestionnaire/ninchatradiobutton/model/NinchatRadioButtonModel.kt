39e24900c83953222c5cb4672d29e470a44a8608
pallab-gain <pallab.gain@gmail.com>
2020-10-28 09:43:04 +0200

    NinchatRadioButtonView  - Data class to hold button model attributes and states

---
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatquestionnaire/ninchatradiobutton/model/NinchatRadioButtonModel.kt
@@ -0,0 +1,14 @@
+package com.ninchat.sdk.ninchatquestionnaire.ninchatradiobutton.model
+
+import com.ninchat.sdk.helper.questionnaire.NinchatQuestionnaireItemGetter
+import org.json.JSONObject
+
+data class NinchatRadioButtonModel(
+        var label: String? = "",
+        var hasError: Boolean? = false,
+        var isSelected: Boolean? = false,
+) {
+    fun parse(jsonObject: JSONObject?) {
+        label = NinchatQuestionnaireItemGetter.getLabel(jsonObject)
+    }
+}
