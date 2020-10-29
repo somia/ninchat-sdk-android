ffd3dbbbe34aee075a9eb2eeb44bc8dba1d1bc77
pallab-gain <pallab.gain@gmail.com>
2020-10-30 00:02:42 +0200

    Moved NinchatRadioButton to MVP pattern   - Unit test Skeleton class for RadioButtonList presenter

---
+++ ninchatsdk/src/test/java/com/ninchat/sdk/ninchatquestionnaire/ninchatradiobuttonlist/presenter/NinchatRadioButtonListPresenterTest.kt
@@ -0,0 +1,31 @@
+package com.ninchat.sdk.ninchatquestionnaire.ninchatradiobuttonlist.presenter
+
+import org.junit.Test
+
+class NinchatRadioButtonListPresenterTest {
+    @Test
+    fun `should render form view`() {
+
+    }
+
+    @Test
+    fun `should render conversation view`() {
+
+    }
+
+    @Test
+    fun `should handle toggle option and toggle data model`() {
+
+    }
+
+    @Test
+    fun `should return isSelected true`() {
+
+    }
+
+    @Test
+    fun `should return isSelected false`() {
+
+    }
+
+}
