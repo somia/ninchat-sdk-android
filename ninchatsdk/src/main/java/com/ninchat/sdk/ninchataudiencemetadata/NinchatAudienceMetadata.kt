20b1b2ee4dd8240f0a42f6a0138aac005a88b7e4
pallab-gain <pallab.gain@gmail.com>
2020-11-09 09:22:02 +0200

    Ninchat audience metadata with cache access

--- ninchatsdk/src/main/java/com/ninchat/sdk/ninchataudiencemetadata/NinchatAudienceMetadata.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchataudiencemetadata/NinchatAudienceMetadata.kt
@@ -4,13 +4,20 @@ import com.ninchat.client.Props
 import com.ninchat.sdk.NinchatSessionManager
 import com.ninchat.sdk.helper.propsparser.NinchatPropsParser
 
-class NinchatAudienceMetadata {
-    private var audienceMetadata: Props? = null
-        set(value: Props?) {
-            field = value
-            // save audience metadata in persistence storage
-            NinchatSessionManager.getInstance()?.context?.let {
-                NinchatPropsParser.getAudienceMetadata(props = value)
-            }
+data class NinchatAudienceMetadata(private var audienceMetadata: Props? = null) {
+    fun update(props: Props?) {
+        audienceMetadata = props
+        // save audience metadata in persistence storage
+        NinchatSessionManager.getInstance()?.context?.let {
+            val metadata = NinchatPropsParser.getAudienceMetadata(props = props)
         }
+    }
+
+    fun get(): Props? {
+        return audienceMetadata
+    }
+
+    fun has(): Boolean {
+        return audienceMetadata != null
+    }
 }
