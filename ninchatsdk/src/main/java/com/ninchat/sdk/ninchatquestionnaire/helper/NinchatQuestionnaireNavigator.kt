11dc5215017f108e2ed73de005f3e1dd974193ab
pallab-gain <pallab.gain@gmail.com>
2020-12-30 14:06:24 +0200

    Update animation navigation

--- ninchatsdk/src/main/java/com/ninchat/sdk/ninchatquestionnaire/helper/NinchatQuestionnaireNavigator.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatquestionnaire/helper/NinchatQuestionnaireNavigator.kt
@@ -1,9 +1,13 @@
 package com.ninchat.sdk.ninchatquestionnaire.helper
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.view.View
 import org.json.JSONObject
 
 class NinchatQuestionnaireNavigator {
     companion object {
+        private const val DURATION: Long = 500;
 
         /**
          * Get the next immediate element that is greater than given element index
@@ -19,7 +23,21 @@ class NinchatQuestionnaireNavigator {
          */
         fun getElementIndex(questionnaireList: List<JSONObject> = listOf(), elementName: String): Int {
             return questionnaireList
-                    .indexOfFirst { it.optString("name") == elementName}
+                    .indexOfFirst { it.optString("name") == elementName }
+        }
+
+        /**
+         * Show smooth animation during loading
+         */
+        fun setAnimation(itemView: View, position: Int, notFirstItem: Boolean) {
+            itemView.alpha = 0.0f
+            val animatorSet = AnimatorSet()
+            val animator: ObjectAnimator = ObjectAnimator.ofFloat(itemView, "alpha", 0f, 0.5f, 1.0f)
+            ObjectAnimator.ofFloat(itemView, "alpha", 0f).start()
+            animator.startDelay = if (notFirstItem) DURATION / 2 else position * DURATION / 3
+            animator.duration = DURATION
+            animatorSet.play(animator)
+            animator.start()
         }
     }
 }
