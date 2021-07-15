3c868f2543af966d041105d03f8555ed23687575
pallab-gain <pallab.gain@gmail.com>
2021-07-15 14:47:35 +0300

    checkbox: add typeface support

--- ninchatsdk/src/main/java/com/ninchat/sdk/ninchatquestionnaire/ninchatcheckbox/view/NinchatCheckboxViewHolder.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatquestionnaire/ninchatcheckbox/view/NinchatCheckboxViewHolder.kt
@@ -1,5 +1,6 @@
 package com.ninchat.sdk.ninchatquestionnaire.ninchatcheckbox.view
 
+import android.graphics.Typeface
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.paris.extensions.style
@@ -54,6 +55,8 @@ class NinchatCheckboxViewHolder(
     }
 
     override fun onUpdateView(isChecked: Boolean, hasError: Boolean, enabled: Boolean) {
+        val value =
+            itemView.resources.getString(R.string.ninchat_questionnaire_checkbox_label_text_style)
         itemView.isEnabled = enabled
         itemView.ninchat_checkbox_label.isEnabled = enabled
         itemView.ninchat_checkbox_label.setTextAppearance(
@@ -64,6 +67,12 @@ class NinchatCheckboxViewHolder(
                     R.style.NinchatTheme_Questionnaire_Checkbox_Label
             }
         )
+        itemView.ninchat_checkbox_label.setTypeface(
+            itemView.ninchat_checkbox_label.typeface, when {
+                value.equals("bold", ignoreCase = true) -> Typeface.BOLD
+                else -> Typeface.NORMAL
+            }
+        )
         itemView.ninchat_checkbox_image.isEnabled = enabled
         itemView.ninchat_checkbox_image.style(
             if (isChecked) R.style.NinchatTheme_Questionnaire_Checkbox_Image_Selected else R.style.NinchatTheme_Questionnaire_Checkbox_Image
@@ -71,12 +80,20 @@ class NinchatCheckboxViewHolder(
     }
 
     override fun onToggleView(isChecked: Boolean, hasError: Boolean, enabled: Boolean) {
+        val value =
+            itemView.resources.getString(R.string.ninchat_questionnaire_checkbox_label_text_style)
         itemView.ninchat_checkbox_label.setTextAppearance(
             when {
                 isChecked -> R.style.NinchatTheme_Questionnaire_Checkbox_Label_Selected
                 enabled -> R.style.NinchatTheme_Questionnaire_Checkbox_Label_Focused
                 else ->
                     R.style.NinchatTheme_Questionnaire_Checkbox_Label
+            }
+        )
+        itemView.ninchat_checkbox_label.setTypeface(
+            itemView.ninchat_checkbox_label.typeface, when {
+                value.equals("bold", ignoreCase = true) -> Typeface.BOLD
+                else -> Typeface.NORMAL
             }
         )
         itemView.ninchat_checkbox_image.isEnabled = enabled
