40b00e153236750aa4442386196548ed77319b23
pallab-gain <pallab.gain@gmail.com>
2022-03-10 02:37:27 +0200

    Update logic to handle media preview during media download

--- ninchatsdk/src/main/java/com/ninchat/sdk/helper/glidewrapper/GlideWrapper.java
+++ ninchatsdk/src/main/java/com/ninchat/sdk/helper/glidewrapper/GlideWrapper.java
@@ -1,20 +1,46 @@
 package com.ninchat.sdk.helper.glidewrapper;
 
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.ninchat.sdk.GlideApp;
-import com.ninchat.sdk.R;
 import com.ninchat.sdk.helper.NinchatImageGetter;
+import com.ninchat.sdk.ninchatmedia.presenter.INinchatMediaCallback;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
 import androidx.annotation.DrawableRes;
+import androidx.annotation.Nullable;
 
 public class GlideWrapper {
     public static void loadImage(Context context, String fileUrl, ImageView imageView) {
         GlideApp.with(context)
                 .load(fileUrl)
                 .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                .into(imageView);
+    }
+
+    public static void loadImage(Context context, String fileUrl, ImageView imageView, INinchatMediaCallback callback) {
+        GlideApp.with(context)
+                .load(fileUrl)
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        callback.onLoadError();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        callback.onLoadSuccess();
+                        return false;
+                    }
+                })
                 .into(imageView);
     }
 
