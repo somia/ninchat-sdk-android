f02bf288af31628f39a724de7d9c70ba236390af
pallab-gain <pallab.gain@gmail.com>
2022-03-17 04:51:26 +0200

    Bring separate P2P and group video view logic handler class

---
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatintegrations/jitsi/NinchatJitsiIntegration.kt
@@ -0,0 +1,64 @@
+package com.ninchat.sdk.ninchatintegrations.jitsi
+
+import android.view.View
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.ninchat_video_view.view.*
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+import org.jitsi.meet.sdk.JitsiMeetView
+import java.net.URL
+
+class NinchatJitsiIntegration(val videoContainer: View, val view: JitsiMeetView? = null) {
+    var options: JitsiMeetConferenceOptions? = null
+
+    // Current only one message type
+    fun handleWebRTCMessage(
+        jitsiVideoView: FrameLayout,
+        serverAddress: String,
+        jitsiRoom: String?,
+        jitsiToken: String?,
+        width: Int,
+        height: Int
+    ) {
+        val jitsiServerAddress = "https://${"jitsi-www"}.${serverAddress.removePrefix("api.")}"
+        this.options = JitsiMeetConferenceOptions.Builder()
+            .setServerURL(URL(jitsiServerAddress))
+            .setRoom(jitsiRoom)
+            .setToken(jitsiToken)
+            .setFeatureFlag("overflow-menu.enabled", true)
+            .setFeatureFlag("add-people.enabled", false)
+            .setFeatureFlag("calendar.enabled", false)
+            .setFeatureFlag("close-captions.enabled", false)
+            .setFeatureFlag("chat.enabled", false)
+            .setFeatureFlag("filmstrip.enabled", false)
+            .setFeatureFlag("invite.enabled", false)
+            .setFeatureFlag("kick-out.enabled", false)
+            .setFeatureFlag("live-streaming.enabled", false)
+            .setFeatureFlag("meeting-name.enabled", true)
+            .setFeatureFlag("meeting-password.enabled", false)
+            .setFeatureFlag("notifications.enabled", false)
+            .setFeatureFlag("recording.enabled", false)
+            .setFeatureFlag("welcomepage.enabled", true)
+            .setFeatureFlag("video-share.enabled", false)
+            .setFeatureFlag("toolbox.alwaysVisible", true)
+            .setFeatureFlag("fullscreen.enabled'", true)
+            .setFeatureFlag("help.enabled", false)
+            .setFeatureFlag("lobby-mode.enabled", false)
+            .setFeatureFlag("reactions.enabled", false)
+            .build()
+
+        view!!.join(options)
+        jitsiVideoView.addView(view, width, height)
+    }
+
+    fun onDestroy() {
+        videoContainer.visibility = View.GONE
+        videoContainer.ninchat_jitsi_layout.visibility = View.GONE
+        view?.leave()
+    }
+
+    fun hangUp() {
+        videoContainer.visibility = View.GONE
+        videoContainer.ninchat_jitsi_layout.visibility = View.GONE
+        view?.leave()
+    }
+}
---
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatintegrations/p2p/NinchatP2PIntegration.kt
@@ -0,0 +1,32 @@
+package com.ninchat.sdk.ninchatintegrations.p2p
+
+import android.view.View
+import com.ninchat.sdk.utils.messagetype.NinchatMessageTypes
+import com.ninchat.sdk.views.NinchatWebRTCView
+import kotlinx.android.synthetic.main.ninchat_video_view.view.*
+
+
+class NinchatP2PIntegration(view: View, onToggleFullScreen: () -> Unit) {
+    val webRTCView: NinchatWebRTCView by lazy {
+        NinchatWebRTCView(view)
+    }
+
+    init {
+        view.fullscreen_on_off.setOnClickListener { onToggleFullScreen() }
+        view.audio_on_off.setOnClickListener { webRTCView.toggleAudio() }
+        view.microphone_on_off.setOnClickListener { webRTCView.toggleMicrophone() }
+        view.video_on_off.setOnClickListener { webRTCView.toggleVideo() }
+        view.hangup_video.setOnClickListener { webRTCView.hangUp() }
+    }
+
+    fun handleRTCMessage(messageType: String?, payload: String?, onHandUp: () -> Unit) {
+        webRTCView.handleWebRTCMessage(messageType, payload)
+        if (NinchatMessageTypes.HANG_UP == messageType) {
+            onHandUp()
+        }
+    }
+
+    fun call() = webRTCView.call()
+    fun hangUp() = webRTCView.hangUp()
+    fun onDestroy() = webRTCView.hangUp()
+}
