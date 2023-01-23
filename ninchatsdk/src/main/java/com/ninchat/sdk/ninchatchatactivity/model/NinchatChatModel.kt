7f70a0f2e6669b71e5c284ae498768eb00d94439
pallab-gain-tt <pallab.gain@terveystalo.com>
2023-01-23 12:50:08 +0200

    NinchatChatModel: remove un-used variables

--- ninchatsdk/src/main/java/com/ninchat/sdk/ninchatchatactivity/model/NinchatChatModel.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatchatactivity/model/NinchatChatModel.kt
@@ -3,5 +3,4 @@ package com.ninchat.sdk.ninchatchatactivity.model
 data class NinchatChatModel(
     var historyLoaded: Boolean = false,
     var toggleFullScreen: Boolean = false,
-    var rootViewHeight: Int = 0,
 )
