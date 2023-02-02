f8649c58382c0bbe1da51a8c1774b75b007fec5d
pallab-gain-tt <pallab.gain@terveystalo.com>
2023-02-02 16:08:48 +0200

    NinchatChatModel: update modal

--- ninchatsdk/src/main/java/com/ninchat/sdk/ninchatchatactivity/model/NinchatChatModel.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/ninchatchatactivity/model/NinchatChatModel.kt
@@ -3,4 +3,5 @@ package com.ninchat.sdk.ninchatchatactivity.model
 data class NinchatChatModel(
     var historyLoaded: Boolean = false,
     var toggleFullScreen: Boolean = false,
+    var chatClosed: Boolean = false,
 )
