e3e416498205413562ef59dd0720b4dd12819314
pallab-gain-tt <pallab.gain@terveystalo.com>
2023-02-13 13:02:20 +0200

    join conference/data: translation and view text for join conference view

--- ninchatsdk/src/main/java/com/ninchat/sdk/helper/siteconfigparser/NinchatSiteConfig.kt
+++ ninchatsdk/src/main/java/com/ninchat/sdk/helper/siteconfigparser/NinchatSiteConfig.kt
@@ -305,6 +305,15 @@ class NinchatSiteConfig {
     fun getTitlebarCloseText(): String =
         getTranslation("Close")
 
+    fun getConferenceTitleText(): String =
+        getTranslation("Video meeting")
+
+    fun getConferenceButtonText(): String =
+        getTranslation("Join video meeting")
+
+    fun getConferenceDescriptionText(): String =
+        getTranslation("Continue to join video meeting. You can toggle text chat from the top.")
+
     internal fun getTranslation(translationKey: String = "translations", key: String): String? {
         var value: String? = null
         siteConfig?.let { config ->
